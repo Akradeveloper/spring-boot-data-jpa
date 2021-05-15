1a2aa9f64b1db7672523894eb6df6d5c168d234f
Akradeveloper <Akradeveloper@gmail.com>
2021-05-15 19:34:09 +0200

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES(1,'Andres','Guzman','email@email.com','2017-08-28');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES(2,'Endika','Santamaria','email2@email2.com','2008-01-14');
