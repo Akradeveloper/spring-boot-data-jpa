71366f28c3db5808fa0244e88d59dc978bcc5fb6
Endika Santamaria <endika.santamaria@stubhub.com>
2021-05-17 18:45:29 +0200

    (PagingAndSortingRepository-Parte-I

--- src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
@@ -3,13 +3,13 @@ package com.bolsadeideas.springboot.app.controller;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,9 +24,11 @@ public class ClienteController {
     private IClienteService clienteService;
 
     @RequestMapping(value = "/listar",method = RequestMethod.GET)
-    public String listar(Model model){
+    public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model){
+        Pageable pageRequest =  PageRequest.of(page,4);
+        Page<Cliente> clientes = clienteService.findAll(pageRequest);
         model.addAttribute("titulo","Listado de clientes");
-        model.addAttribute("clientes", clienteService.findAll());
+        model.addAttribute("clientes",clientes);
         return "listar";
     }
 
